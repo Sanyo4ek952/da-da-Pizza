b59bf88dd21948b3d9ab6a5ef91fd18502ac715a
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-07-06 18:55:09 +0300

    init: first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type {Metadata} from "next";
+import {Nunito} from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+    subsets: ['cyrillic'],
+    variable: '--font-nunito',
+    weight: ['400', '500', '600', '700', '800', '900'],
+});
+export const metadata: Metadata = {
+    title: "Da Da Pizza | Главная",
+    description: "Pizza",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <body className={`${nunito.variable} antialiased`}>
+        <main
+            className={'min-h-screen'}>{children}</main>
+        </body>
+        </html>
+    );
+}
