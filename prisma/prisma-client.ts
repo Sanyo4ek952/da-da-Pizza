15244f2d3e7073185eb61d65950250bea3ae0ed0
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-08-25 18:37:40 +0300

    refactor: prisma

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client/extension'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+// prisma-client.ts
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+  // Это нужно, чтобы в development не создавать новый экземпляр при каждом хот-релоаде
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+// Используем существующий глобальный prisma или создаём новый
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'], // необязательно, удобно для отладки
+  })
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+// В dev сохраняем в глобальный объект, чтобы Prisma не создавал новый экземпляр каждый раз
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
