7046a473073151abd8ccaddecbc1be148ff087a5
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-08-28 17:04:31 +0300

    feat: add prisma schema

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,16 @@
 import { prisma } from '@/prisma/prisma-client'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
   const users = await prisma.user.findMany()
   return new Response(JSON.stringify(users), { status: 200 })
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json()
+
+  const user = await prisma.user.create({
+    data,
+  })
+  return NextResponse.json(user)
 }
