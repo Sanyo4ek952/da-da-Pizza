3d877d8f21aab0f66cd79698b07c8bad57923b5f
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-09-02 15:25:06 +0300

    feat: pizzaForm into choose-product-modal.tsx

--- components/shared/choose-pizza-form.tsx
+++ components/shared/choose-pizza-form.tsx
@@ -1,8 +1,40 @@
 import { FC } from 'react'
+import { Ingredient, ProductItem } from '@prisma/client'
+import { cn } from '@/lib/utils'
+import { ProductImage } from '@/components/shared/product-image'
+import { Title } from '@/components/shared'
+import { Button } from '@/components/ui'
 
 interface Props {
+  imageUrl: string
+  name: string
+  ingredients: Ingredient[]
+  items: ProductItem[]
+  loading?: boolean
+  onSubmit?: (itemId: number, ingredients: number[]) => void
   className?: string
 }
-export const ChoosePizzaForm: FC<Props> = ({ className }) => {
-  return <div className={className}></div>
+export const ChoosePizzaForm: FC<Props> = ({
+  name,
+  items,
+  imageUrl,
+  ingredients,
+  loading,
+  onSubmit,
+  className,
+}) => {
+  const textDetails = '30 см, традиционное тесто 30'
+  const totalPrice = 350
+  return (
+    <div className={cn(className, 'flex flex-1')}>
+      <ProductImage imageUrl={imageUrl} size={30} />
+      <div className="w-[490px] bg-[#f7f6f5] p-7">
+        <Title text={name} size={'md'} className={'font-extrabold mb-1'} />
+        <p className={'text-gray-400'}>{textDetails}</p>
+        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
+          Добавить в корзину за {totalPrice} ₽
+        </Button>
+      </div>
+    </div>
+  )
 }
--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -2,7 +2,7 @@
 import { FC } from 'react'
 import { cn } from '@/lib/utils'
 import { ProductWithRelations } from '@/@types/prisma'
-import { Title } from '@/components/shared'
+import { ChoosePizzaForm } from '@/components/shared'
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { useRouter } from 'next/navigation'
 
@@ -20,7 +20,12 @@ export const ChooseProductModal: FC<Props> = ({ className, product }) => {
           'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden'
         )}
       >
-        <Title text={product.name} />
+        <ChoosePizzaForm
+          items={product.items}
+          imageUrl={product.imageUrl}
+          name={product.name}
+          ingredients={product.ingredients}
+        />
       </DialogContent>
     </Dialog>
   )
