82f0c241b9f9fb098fde33a419d780308dddc409
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-09-17 12:26:16 +0300

    refactor: props product page

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -5,9 +5,9 @@ import {prisma} from '@/prisma/prisma-client';
 export default async function ProductModalPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
   const product = await prisma.product.findFirst({
     where: { id: Number(id) },
     include: {
