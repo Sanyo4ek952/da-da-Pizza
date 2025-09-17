e955b085065d7a4bc285bb003ea7d322078585f5
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-09-17 13:08:43 +0300

    refactor: props product page

--- app/api/cart/[id]/route.ts
+++ app/api/cart/[id]/route.ts
@@ -4,28 +4,32 @@ import { updateCartTotalAmount } from '@/shared/lib/update-cart-total-amount';
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = await params;
+    const { id } = await context.params;
     const data = (await req.json()) as { quantity: number };
     const token = req.cookies.get('cartToken')?.value;
+
     if (!token) {
       return NextResponse.json({ error: 'Cart token not found' });
     }
+
     const cartItem = await prisma.cartItem.findFirst({
       where: {
         id: Number(id),
       },
     });
+
     if (!cartItem) {
-      return NextResponse.json({ error: 'Cart token not found' });
+      return NextResponse.json({ error: 'Cart item not found' });
     }
 
     await prisma.cartItem.update({
       where: { id: Number(id) },
       data: { quantity: data.quantity },
     });
+
     const updatedUserCart = await updateCartTotalAmount(token);
     return NextResponse.json(updatedUserCart);
   } catch (error) {
@@ -36,30 +40,34 @@ export async function PATCH(
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = await params;
+    const { id } = await context.params;
     const token = req.cookies.get('cartToken')?.value;
 
     if (!token) {
       return NextResponse.json({ error: 'Cart token not found' });
     }
+
     const cartItem = await prisma.cartItem.findFirst({
       where: {
         id: Number(id),
       },
     });
+
     if (!cartItem) {
-      return NextResponse.json({ error: 'Cart token not found' });
+      return NextResponse.json({ error: 'Cart item not found' });
     }
+
     await prisma.cartItem.delete({
       where: { id: Number(id) },
     });
+
     const updatedUserCart = await updateCartTotalAmount(token);
     return NextResponse.json(updatedUserCart);
   } catch (error) {
-    console.log('[CART_PATCH_ERROR] Server error', error);
+    console.log('[CART_DELETE_ERROR] Server error', error);
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
