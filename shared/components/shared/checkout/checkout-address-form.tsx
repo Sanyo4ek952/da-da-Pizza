0646b5837a05f27124be0f6c95af1541a75ebe21
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-09-26 16:34:08 +0300

    fix: address form

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -3,32 +3,29 @@
 import React from 'react';
 import { WhiteBlock } from '../white-block';
 import { FormTextarea } from '../form';
-import { Controller, useFormContext } from 'react-hook-form';
-import { ErrorText } from '../error-text';
-import AddressInputClient from '@/shared/components/shared/addres-input-client';
 
 interface Props {
   className?: string;
 }
 
 export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
-  const { control } = useFormContext();
+  // const { control } = useFormContext();
 
   return (
     <WhiteBlock title="3. Адрес доставки" className={className}>
       <div className="flex flex-col gap-5">
-        <Controller
-          control={control}
-          name="address"
-          render={({ field, fieldState }) => (
-            <>
-              <AddressInputClient onChange={field.onChange} />
-              {fieldState.error?.message && (
-                <ErrorText text={fieldState.error.message} />
-              )}
-            </>
-          )}
-        />
+        {/*<Controller*/}
+        {/*  control={control}*/}
+        {/*  name="address"*/}
+        {/*  render={({ field, fieldState }) => (*/}
+        {/*    <>*/}
+        {/*      <AddressInputClient onChange={field.onChange} />*/}
+        {/*      {fieldState.error?.message && (*/}
+        {/*        <ErrorText text={fieldState.error.message} />*/}
+        {/*      )}*/}
+        {/*    </>*/}
+        {/*  )}*/}
+        {/*/>*/}
 
         <FormTextarea
           name="comment"
