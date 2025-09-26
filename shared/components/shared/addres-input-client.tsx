f27a6b5c8f24e8dcbee002673f2d03ee77ea4b1c
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-09-26 16:06:59 +0300

    fix: address-input-client

---
+++ shared/components/shared/addres-input-client.tsx
@@ -0,0 +1,14 @@
+'use client';
+import dynamic from 'next/dynamic';
+
+const AddressInput = dynamic(
+  () => import('@/shared/components').then((mod) => mod.AddressInput),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="h-12 bg-gray-100 animate-pulse rounded"></div>
+    ),
+  }
+);
+
+export default AddressInput;
