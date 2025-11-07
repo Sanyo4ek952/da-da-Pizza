f0fc8f4c4f19466b590f534196503e2692837832
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-11-07 19:25:53 +0300

    refactor: hidden block stories

--- shared/components/shared/stories.tsx
+++ shared/components/shared/stories.tsx
@@ -39,7 +39,7 @@ export const Stories: React.FC<Props> = ({ className }) => {
     <>
       <Container
         className={cn(
-          'flex items-center justify-between gap-2 my-10',
+          'flex overflow-hidden items-center justify-between gap-2 my-10',
           className
         )}
       >
