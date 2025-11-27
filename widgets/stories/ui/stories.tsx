0551e6d89653e749358bfa99aee91d095b68c8b2
Sanyo4ek958 <sanyo4ek952@gmail.com>
2025-11-27 16:15:01 +0300

    refactor: swiper stories create

--- widgets/stories/ui/stories.tsx
+++ widgets/stories/ui/stories.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { IStory } from '@shared/api/stories';
 import React from 'react';
-import { Container } from '@shared/ui';
-import { cn } from '@shared/lib/utils';
-import Image from 'next/image';
 import { useStories } from '@shared/hooks/use-stories';
+import { IStory } from '@shared/api/stories';
+import SwiperStories from '@widgets/stories/ui/swiper-stories';
 import { ItemStory } from '@widgets/stories/ui/item-story';
+import { Container } from '@shared/ui';
 
 interface Props {
   className?: string;
@@ -16,7 +15,6 @@ export const Stories: React.FC<Props> = ({ className }) => {
   const { stories } = useStories();
   const [open, setOpen] = React.useState(false);
   const [selectedStory, setSelectedStory] = React.useState<IStory>();
-
   const onClickStory = (story: IStory) => {
     setSelectedStory(story);
 
@@ -24,42 +22,24 @@ export const Stories: React.FC<Props> = ({ className }) => {
       setOpen(true);
     }
   };
-
   return (
     <>
-      <Container
-        className={cn(
-          'flex overflow-hidden items-center justify-between gap-2 my-10',
-          className
-        )}
-      >
-        {stories.length === 0 &&
-          [...Array(6)].map((_, index) => (
-            <div
-              key={index}
-              className="w-[200px] h-[250px] bg-gray-200 rounded-md animate-pulse"
-            />
-          ))}
-
-        {stories.map((story) => (
-          <Image
-            alt={'story'}
-            key={story.id}
-            onClick={() => onClickStory(story)}
-            className="rounded-md cursor-pointer"
-            height={250}
-            width={200}
-            src={story.previewImageUrl}
+      {stories.length === 0 &&
+        [...Array(6)].map((_, index) => (
+          <div
+            key={index}
+            className="w-[200px] h-[250px] bg-gray-200 rounded-md animate-pulse"
           />
         ))}
-
-        {open && selectedStory && (
-          <ItemStory
-            selectedStory={selectedStory}
-            closeStory={() => setOpen(false)}
-          />
-        )}
+      <Container className={className}>
+        <SwiperStories stories={stories} onClickStory={onClickStory} />
       </Container>
+      {open && selectedStory && (
+        <ItemStory
+          selectedStory={selectedStory}
+          closeStory={() => setOpen(false)}
+        />
+      )}
     </>
   );
 };
---
+++ widgets/stories/ui/swiper-stories.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { IStory } from '@shared/api/stories';
+import Image from 'next/image';
+import 'swiper/css';
+
+interface Props {
+  className?: string;
+  stories: IStory[];
+  onClickStory: (story: IStory) => void;
+}
+
+const SwiperStories = ({ stories, onClickStory }: Props) => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={4}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      className={'flex'}
+      breakpoints={{
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 10,
+        },
+      }}
+    >
+      {stories.map((story) => (
+        <SwiperSlide key={story.id}>
+          <Image
+            alt={'story'}
+            onClick={() => onClickStory(story)}
+            className="rounded-md cursor-pointer"
+            height={250}
+            width={200}
+            src={story.previewImageUrl}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwiperStories;
